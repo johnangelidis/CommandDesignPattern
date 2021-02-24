97b78ad40f032d1d6e75292f0884207f081af2ac
johnangelidis <59876191+johnangelidis@users.noreply.github.com>
2021-02-24 18:56:09 -0500

    Completed Robot class

--- Robot.java
+++ Robot.java
@@ -1,3 +1,18 @@
 public class Robot {
-    
+    public Robot(String name){
+        System.out.println("Welcome, my name is " + ", I'm a friendly robot.");
+    }
+
+    public void pickup(){
+        System.out.println("Oh treasure, picking it up!")
+    }
+    public void jump(){
+        System.out.println("Oh no a hazard, I'm jumping over it.");
+    }
+    public void fire(){
+        System.out.println("Bad guys! Fire my blow torch at them.");
+    }
+    public void heal(){
+        System.out.println("Thanks for healing my wounds.");
+    }
 }
