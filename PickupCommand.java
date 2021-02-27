756ee9933d51d2db4856d61b14db9654e85d8c1f
Coby Arambula <cobyarambula@gmail.com>
2021-02-27 14:40:18 -0500

    added coded and JavaDoc

--- PickupCommand.java
+++ PickupCommand.java
@@ -1,9 +1,25 @@
+/**
+ * Class representation of pickup command executed by the Robot class
+ * @author Coby Arambula
+ */
 public class PickupCommand implements Command {
+    private Robot robot;
 
+    /**
+     * Parameterized constructor for the PickupCommand class
+     * @param robot instance of the robot class
+     */
+    public PickupCommand(Robot robot) {
+        this.robot = robot;
+    }
+
+    /**
+     * Execute method that calls the pickup() method from the
+     * Robot class
+     */
     @Override
     public void execute() {
-        // TODO Auto-generated method stub
-
+        robot.pickup();
     }
     
 }
