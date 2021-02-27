982b0bc7da0798129c3c3ff964b883d583e081fe
johnangelidis <59876191+johnangelidis@users.noreply.github.com>
2021-02-27 15:18:36 -0500

    Added code and comments

--- JumpCommand.java
+++ JumpCommand.java
@@ -1,8 +1,22 @@
+/**
+ * Class that represents a robot jumping
+ * @author John Angelidis
+ */
 public class JumpCommand implements Command {
-
+    private Robot robot;
+    /**
+     * Constructor for a jump command
+     * @param robot of type Robot
+     */
+    public JumpCommand(Robot robot){
+        this.robot = robot;
+    }
+    /**
+     * Method that executes a jump command
+     */
     @Override
     public void execute() {
-        // TODO Auto-generated method stub
+        robot.jump();
 
     }
     
