5f9aa4443d446d2d32d846bc54253fcf9616b9b1
challison404 <challison404@users.noreply.github.com>
2021-02-27 14:25:03 -0500

    Added output to FireCommand

--- FireCommand.java
+++ FireCommand.java
@@ -1,9 +1,12 @@
 public class FireCommand implements Command {
 
     @Override
+    /*
+    *Execute will print out that the robot is attacking
+    */
     public void execute() {
         // TODO Auto-generated method stub
-
+        System.out.println("Bad guys! Firing my blow torch at them.");
     }
-    
+
 }
