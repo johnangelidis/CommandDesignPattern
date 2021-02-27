622b6bc47be6e8e4aaa153529aeab02c25c3c777
Coby Arambula <cobyarambula@gmail.com>
2021-02-27 16:43:08 -0500

    fixed InputHandler

--- InputHandler.java
+++ InputHandler.java
@@ -1,20 +1,17 @@
 import java.util.HashMap;
-import java.util.Map;
-
-import javax.lang.model.util.ElementScanner14;
 /**
  * Contains a hashmap for commands and a means to call them
  * @author Coby Arambula
  */
 public class InputHandler {
-    
+    HashMap<String, Command> command;
     /**
      * Parameterized constructor for the InputHandler class
      * that initializes a HashMap that contains each command
      * @param robot instance of the Robot class
      */
     public InputHandler(Robot robot) {
-        HashMap<String, Command> command = new HashMap<String, Command>();
+        command = new HashMap<String, Command>();
         var pC = new PickupCommand(robot);
         var jC = new JumpCommand(robot);
         var fC = new FireCommand(robot);
@@ -32,18 +29,12 @@ public class InputHandler {
      * @param data string representation of hashmap key
      */
     public void inputEntered(String data) {
-        Robot robot = new Robot("robot");
-        if (data.equalsIgnoreCase("pickup"))
-            robot.pickup();
-        else if (data.equalsIgnoreCase("jump"))
-            robot.jump();
-        else if (data.equalsIgnoreCase("fire"))
-            robot.fire();
-        else if (data.equalsIgnoreCase("jump"))
-            robot.heal();
-        else {
+        if ((data.equalsIgnoreCase("pickup") || data.equalsIgnoreCase("jump") ||
+                                                data.equalsIgnoreCase("fire") || data.equalsIgnoreCase("heal")))
+            command.get(data).execute();
+        else
             System.out.println("An invalid command was entered");
-        }
     }
-
 }
+
+
