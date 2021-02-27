7e346b6653f8379622a87ee63b4b39ee01b1f64c
Jayxden <77359383+Jayxden@users.noreply.github.com>
2021-02-27 15:50:32 -0500

    Heal Command Class/ added Comments

--- HealCommand.java
+++ HealCommand.java
@@ -1,10 +1,19 @@
+/* 
+* Class represents a robot healing.
+* @author Jayden Allen
+*/
 public class HealCommand implements Command{
     private Robot robot;
-
+    /**
+     * Constructor for a Heal Command
+     * @param robot of type Robot
+     */
     public HealCommand(Robot robot){
         this.robot = robot;
     }
-
+    /**
+     * Method that execuetes a jump command.
+     */
     @author
     public void execute() {
         // TODO Auto-generated method stub
